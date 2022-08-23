5467ddac6c5ec33c5c5d29c2f03d04e486c34bd0
guic369 <guimestreyoda@outlook.com>
2022-08-22 21:11:30 -0300

    classe agendamento

---
+++ OneToManyEngenharia/src/model/Agendamento.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+public class Agendamento {
+    private int codAgendamento;
+    private Empresa codEmpresa;
+    private Cliente codCliente;
+    private LocalDateTime horario;
+    private LocalDate data;
+
+    public Agendamento() {
+    }
+
+    public Agendamento(int codAgendamento, Empresa codEmpresa, Cliente codCliente, LocalDateTime horario, LocalDate data) {
+        this.codAgendamento = codAgendamento;
+        this.codEmpresa = codEmpresa;
+        this.codCliente = codCliente;
+        this.horario = horario;
+        this.data = data;
+    }
+
+    public int getCodAgendamento() {
+        return codAgendamento;
+    }
+
+    public void setCodAgendamento(int codAgendamento) {
+        this.codAgendamento = codAgendamento;
+    }
+
+    public Empresa getCodEmpresa() {
+        return codEmpresa;
+    }
+
+    public void setCodEmpresa(Empresa codEmpresa) {
+        this.codEmpresa = codEmpresa;
+    }
+
+    public Cliente getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(Cliente codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public LocalDateTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalDateTime horario) {
+        this.horario = horario;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    
+    
+    
+}
