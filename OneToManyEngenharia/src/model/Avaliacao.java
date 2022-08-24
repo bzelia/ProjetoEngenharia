3e3fbdd3f031274531498936a80546b332681d45
Joao-Daniel <joaomilagres982@gmail.com>
2022-08-24 19:23:12 -0300

    Alteração das Variaveis 'Servico', 'Cliente' e 'localData'

--- OneToManyEngenharia/src/model/Avaliacao.java
+++ OneToManyEngenharia/src/model/Avaliacao.java
@@ -1,56 +1,40 @@
 package model;
 
 public class Avaliacao {
-    private Servico codServico; 
-    private Cliente codCliente;
+    private Servico servico; 
+    private Cliente cliente;
     private int notaAvaliacao;
-    private int dataAvaliacao;
+    private int localDate;
     private String comentario;
 
     public Avaliacao() {}
 
-    public Avaliacao(String codServico, String codCliente, int notaAvaliacao, int dataAvaliacao, String comentario) {
-        this.codServico = codServico;
-        this.codCliente = codCliente;
+    public Avaliacao(Servico Servico, Cliente Cliente, int notaAvaliacao, int localDate, String comentario) {
+        this.servico = Servico;
+        this.cliente = Cliente;
         this.notaAvaliacao = notaAvaliacao;
-        this.dataAvaliacao = dataAvaliacao;
+        this.localDate = localDate;
         this.comentario = comentario;
-    }
-
-    public String getCodServico() {
-        return codServico;
-    }
-
-    public String getCodCliente() {
-        return codCliente;
     }
 
     public int getNotaAvaliacao() {
         return notaAvaliacao;
     }
 
-    public int getDataAvaliacao() {
-        return dataAvaliacao;
+    public int getLocalDate() {
+        return localDate;
     }
 
     public String getComentario() {
         return comentario;
     }
 
-    public void setCodServico(String codServico) {
-        this.codServico = codServico;
-    }
-
-    public void setCodCliente(String codCliente) {
-        this.codCliente = codCliente;
-    }
-
     public void setNotaAvaliacao(int notaAvaliacao) {
         this.notaAvaliacao = notaAvaliacao;
     }
 
-    public void setDataAvaliacao(int dataAvaliacao) {
-        this.dataAvaliacao = dataAvaliacao;
+    public void setLocalDate(int localDate) {
+        this.localDate = localDate;
     }
 
     public void setComentario(String comentario) {
